c11b700c3b9e8cb24894e3f6e4faa1f7cd6fc948
Dicle <diclebahceli24@gmail.com>
2022-04-10 13:48:50 +0300

    solve question1

---
+++ java/warmUp1/question1.java
@@ -0,0 +1,9 @@
+public boolean sleepIn(boolean weekday, boolean vacation) {
+  if(weekday==false && vacation==false)
+    return true;
+  else if(weekday==true && vacation==false)
+    return false;
+  else
+    return true;
+    
+}
