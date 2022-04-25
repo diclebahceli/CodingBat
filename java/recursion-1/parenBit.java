f9c0188705c3b5b21c8eb92cef40ef785b0fd35b
Dicle <diclebahceli24@gmail.com>
2022-04-25 13:16:30 +0300

    Add a new solution

---
+++ java/recursion-1/parenBit.java
@@ -0,0 +1,7 @@
+public String parenBit(String str) {
+  if(str.charAt(0)!='(')
+    return parenBit(str.substring(1,str.length()));
+  else if(str.charAt(str.length()-1)!=')')
+    return parenBit(str.substring(0,str.length()-1));
+  return str;
+}
