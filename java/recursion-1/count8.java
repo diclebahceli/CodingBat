93c4b1e09bb54e36c78452953262148133841173
Dicle <diclebahceli24@gmail.com>
2022-04-25 12:46:21 +0300

    Add answers

---
+++ java/recursion-1/count8.java
@@ -0,0 +1,9 @@
+public int count8(int n) {
+  if(n==0)
+    return 0;
+  else if(n%100==88)
+    return 2 + count8(n/10);
+  else if(n%10==8)
+    return 1 + count8(n/10);
+  return count8(n/10);
+}
